ce363913a0c8bfbd86360db5a3aada132ab45977
logonrm <logonrm@fiap.com.br>
2017-03-07 11:58:05 -0300

    Adicionado busca de Pratos

--- 05-WebservicesSOAP-requester-desktop/src/br/com/fiap/view/Tela.java
+++ 05-WebservicesSOAP-requester-desktop/src/br/com/fiap/view/Tela.java
@@ -5,6 +5,8 @@ import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 
 import br.com.fiap.bo.PratoBOStub;
+import br.com.fiap.bo.PratoBOStub.Buscar;
+import br.com.fiap.bo.PratoBOStub.BuscarResponse;
 import br.com.fiap.bo.PratoBOStub.Cadastrar;
 import br.com.fiap.bo.PratoBOStub.Prato;
 
@@ -23,6 +25,10 @@ public class Tela {
 	private Text txtNome;
 	private Text txtDescricao;
 	private Text txtPreco;
+	private Text txtCodigo;
+	private Text txtNomeBusca;
+	private Text txtDescricaoBusca;
+	private Text txtPrecoBusca;
 
 	/**
 	 * Launch the application.
@@ -57,7 +63,7 @@ public class Tela {
 	 */
 	protected void createContents() {
 		shell = new Shell();
-		shell.setSize(194, 300);
+		shell.setSize(452, 300);
 		shell.setText("SWT Application");
 		
 		txtNome = new Text(shell, SWT.BORDER);
@@ -111,6 +117,55 @@ public class Tela {
 		});
 		btnEnviar.setBounds(10, 193, 75, 25);
 		btnEnviar.setText("Enviar");
+		
+		Label lblCodigo = new Label(shell, SWT.NONE);
+		lblCodigo.setBounds(195, 43, 55, 15);
+		lblCodigo.setText("C\u00F3digo");
+		
+		txtCodigo = new Text(shell, SWT.BORDER);
+		txtCodigo.setBounds(269, 40, 76, 21);
+		
+		Button btnBuscar = new Button(shell, SWT.NONE);
+		btnBuscar.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				try{
+					PratoBOStub bo = new PratoBOStub();
+					Buscar params = new Buscar();
+					params.setCodigo(Integer.parseInt(txtCodigo.getText()));
+					BuscarResponse resp = bo.buscar(params);
+					
+					txtNomeBusca.setText(resp.get_return().getNome());
+					txtDescricaoBusca.setText(resp.get_return().getDescricao());
+					txtPrecoBusca.setText(resp.get_return().getPreco()+"");
+				}catch(Exception ex){
+					ex.printStackTrace();
+				}
+			}
+		});
+		btnBuscar.setBounds(351, 38, 75, 25);
+		btnBuscar.setText("Buscar");
+		
+		Label lblNome_1 = new Label(shell, SWT.NONE);
+		lblNome_1.setBounds(195, 92, 55, 15);
+		lblNome_1.setText("Nome");
+		
+		Label lblDescrio = new Label(shell, SWT.NONE);
+		lblDescrio.setBounds(195, 131, 55, 15);
+		lblDescrio.setText("Descri\u00E7\u00E3o");
+		
+		Label lblPreo = new Label(shell, SWT.NONE);
+		lblPreo.setBounds(195, 172, 55, 15);
+		lblPreo.setText("Pre\u00E7o");
+		
+		txtNomeBusca = new Text(shell, SWT.BORDER);
+		txtNomeBusca.setBounds(270, 92, 76, 21);
+		
+		txtDescricaoBusca = new Text(shell, SWT.BORDER);
+		txtDescricaoBusca.setBounds(270, 125, 76, 21);
+		
+		txtPrecoBusca = new Text(shell, SWT.BORDER);
+		txtPrecoBusca.setBounds(270, 166, 76, 21);
 
 		
 	}
